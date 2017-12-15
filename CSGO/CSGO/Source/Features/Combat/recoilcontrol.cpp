0f4abb92261b69b0a0c31632264bd0158e4872b6
voiding <nsbevids@gmail.com>
2017-12-14 23:15:36 -0700

    Added RCS Smoothing based on next shot time
    Kind of ghetto, will improve later if this doesn't work

--- CSGO/CSGO/Source/Features/Combat/recoilcontrol.cpp
+++ CSGO/CSGO/Source/Features/Combat/recoilcontrol.cpp
@@ -2,17 +2,28 @@
 
 void CRecoilControl::RecoilControl( )
 {
-	// TODO smoothing based on next primary attack
-	if ( eng.GetShotsFired( ) > 1 && eng.GetNextPrimaryAttack( ) <= 0.f )
+	if ( eng.GetShotsFired( ) > 1 ) // ignore shot count
 	{
-		angle_t aCurrentAimPunch = eng.GetAimPunch( ) - aOldAimPunch;
-		aCurrentAimPunch *= 2.f;
-		eng.SetViewAngle( eng.GetViewAngle( ) - aCurrentAimPunch );
-		aOldAimPunch = eng.GetAimPunch( );
+		if ( eng.GetNextPrimaryAttack( ) <= 0.f )
+		{
+			eng.WaitTicks( 1 );
+
+			angle_t angCurrentAimPunch = eng.GetAimPunch( ) - angOldAimPunch;
+			angCurrentAimPunch *= 2.f; // rcs factor
+
+			// smoothness factor = 25
+			for ( float flNextPrimaryAttack = eng.GetNextPrimaryAttack( ); flNextPrimaryAttack > 0.f; flNextPrimaryAttack = eng.GetNextPrimaryAttack( ) )
+			{
+				eng.SetViewAngle( eng.GetViewAngle( ) - angCurrentAimPunch / 25.f );
+				Wait( int( flNextPrimaryAttack / .025f ) );
+			}
+
+			angOldAimPunch = eng.GetAimPunch( );
+		}
 	}
-	else if ( !eng.GetShotsFired( ) )
+	else
 	{
-		aOldAimPunch = { 0, 0, 0 };
+		angOldAimPunch = { 0, 0, 0 };
 	}
 	eng.WaitTicks( 1 );
 }
