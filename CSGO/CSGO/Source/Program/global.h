300902c48853b2e3ec448a54f569d1d8eb42eee7
voiding <nsbevids@gmail.com>
2018-01-06 16:09:48 -0700

    Fixed Release Compilation
    Todo P[B-1]ss Args to WinMain from DllMain @atleast3characterslong

--- CSGO/CSGO/Source/Program/global.h
+++ CSGO/CSGO/Source/Program/global.h
@@ -6,14 +6,18 @@ typedef long long moment;
 #define DEBUG LogDebugMsg
 #define LASTERR LogLastError
 #else
-#define DEBUG
-#define LASTERR
+void EmptyFunction( ... )
+{
+	return;
+}
+#define DEBUG EmptyFunction
+#define LASTERR EmptyFunction
 #endif
 #define PI 3.141593f
 #define MILLISECONDS_PER_SECOND 1000.f
 
-#define PREMIUM_USERS unsigned __int8(55)
-#define MESSAGE( Title, Text, Type ) MessageBox( nullptr, Text, Title, (Type) | MB_OK )
+#define PREMIUM_USERS unsigned __int8( 55 )
+#define MESSAGE( Title, Text, Type ) MessageBox( nullptr, Text, Title, ( Type ) | MB_OK )
 #define DECEMBER_FIRST moment( 1512086400000 ) // time since epoch in milliseconds to december first 2017
 #define DAY moment( 8640000 ) // day in milliseconds
 
