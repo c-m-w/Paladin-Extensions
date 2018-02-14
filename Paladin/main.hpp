a7a02346c8e6311cbdb717546dfc726035a533f6
voiding <nsbevids@gmail.com>
2018-02-13 17:55:25 -0700

    Removed Solution until Framework is done

--- Paladin/main.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <J-e-r-e-m-i-a-h/C++ Standard Library.hpp>
-#include <Windows.h>
-#include <nlohmann/json.hpp>
-#include <J-e-r-e-m-i-a-h/Configuration.hpp>
-#define NK_IMPLEMENTATION
-#include <vurtun/nuklear.h>
