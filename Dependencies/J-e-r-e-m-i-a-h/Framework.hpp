a7133f0d00d5417f87830e4582e6c0250dcc2a47
voiding <nsbevids@gmail.com>
2018-02-13 17:55:33 -0700

    Updated Framework

---
+++ Dependencies/J-e-r-e-m-i-a-h/Framework.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "C++ Standard Library.hpp"
+
+#include <Windows.h>
+#include "Paladin Framework/Debugging.hpp"
+
+#include <nlohmann/json.hpp>
+#include "Paladin Framework/Configuration.hpp"
+
+#include <d3d9.h>
+#include "Paladin Framework/Drawing.hpp"
+
+#include <vurtun/nuklear.h>
+#include "Paladin Framework/Menu.hpp"
