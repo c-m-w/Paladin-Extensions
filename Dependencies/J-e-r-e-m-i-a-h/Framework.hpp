22641e93ff426b866b3f4b7025513d85112447be
voiding <nsbevids@gmail.com>
2018-02-16 18:47:57 -0700

    Updated Framework
    Added OpenSSL

--- Dependencies/J-e-r-e-m-i-a-h/Framework.hpp
+++ Dependencies/J-e-r-e-m-i-a-h/Framework.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
-#include "C++ Standard Library.hpp"
-
+#include <C++ Standard Library.hpp>
 #include <Windows.h>
 #include "Paladin Framework/Debugging.hpp"
 
-#include <openssl/openssl.h>
+#include <openssl/aes.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+#include <openssl/ssl.h>
 #include "Paladin Framework/Connectivity.hpp"
 
 #include <nlohmann/json.hpp>
