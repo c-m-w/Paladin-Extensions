6b0d96c52daf9af530b22b512c17cf0d4c909519
voiding <nsbevids@gmail.com>
2018-02-27 19:59:48 -0700

    Updated Framework
    Added automatic entry point creation.

--- Dependencies/J-e-r-e-m-i-a-h/Framework.hpp
+++ Dependencies/J-e-r-e-m-i-a-h/Framework.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "std.hpp"
-#include "Paladin Framework/Debugging.hpp"
+
+#include "Paladin Framework/Debug.hpp"
+#include "Paladin Framework/Entry.hpp"
 #include "Paladin Framework/Encryption.hpp"
 #include "Paladin Framework/Connectivity.hpp"
 #include "Paladin Framework/Configuration.hpp"
---
+++ Dependencies/J-e-r-e-m-i-a-h/Paladin Framework/Entry.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#include "../Framework.hpp"
+
+#ifdef ENTRY_AS_WIN
+
+void Main( );
+
+namespace Paladin
+{
+	HINSTANCE hinstWin;
+};
+
+int WINAPI WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
+{
+	Paladin::hinstWin = hInstance;
+
+	Main( );
+
+	return NULL;
+}
+
+#elseifdef ENTRY_AS_DLL
+
+void Main( );
+
+namespace Paladin
+{
+	HINSTANCE hinstDLL;
+};
+
+DWORD WINAPI ThreadProc( _In_ LPVOID lpParameter )
+{
+	Main( );
+
+	return NULL;
+}
+
+BOOL WINAPI DllMain( _In_ HINSTANCE hinstDLL, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved )
+{
+	switch ( fdwReason )
+	{
+		case DLL_PROCESS_ATTACH:
+		{
+			Paladin::hinstDLL = hinstDLL;
+
+			HANDLE hThreadProc = CreateThread( nullptr, 0, ThreadProc, lpvReserved, 0, nullptr );
+			if ( !hThreadProc || hThreadProc == INVALID_HANDLE_VALUE )
+				return FALSE;
+
+			DisableThreadLibraryCalls( hinstDLL );
+			return TRUE;
+		}
+		case DLL_PROCESS_DETACH:
+		{
+			return TRUE;
+		}
+		default:
+		{
+			return FALSE;
+		}
+	}
+}
+
+#else
+
+#warning "No entry method defined. Use '#define ENTRY_AS_WIN' or '#define ENTRY_AS_DLL' when including the framework to use automatic entry creation."
+
+#endif
