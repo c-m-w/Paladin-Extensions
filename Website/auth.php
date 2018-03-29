9076e4c6416ab0c775c601690b278737a8b1a035
Cole <gtxzombie25@gmail.com>
2018-03-29 09:15:15 -0600

    Added Encryption/Decryption

--- Website/auth.php
+++ Website/auth.php
@@ -2,6 +2,9 @@
 
 require 'include.php';
 
+if( $connection->connect_error )
+	die( ReturnKeys[ "Establishing Failure" ] );
+
 // Todo: xf_loader_atmp logging
 // Todo: DLL Streaming
 // Todo: DLL Compilation (separate website)
@@ -18,14 +21,20 @@ require 'include.php';
 //  xf_user_field_value:    user_id ->    field_value WHERE field_id = "unique_id"
 //  xf_loader_atmp:         *
 
+$identifiers = array( "id" 		=> GenerateIdentifier( "id" ),
+						"ext" 	=> GenerateIdentifier( "ext" ),
+						"sk" 	=> GenerateIdentifier( "sk" ),
+						"uid"	=> GenerateIdentifier( "uid" ) );
+						
+
 $sqlcommands    = array( "xf_user"
-                                => 'SELECT secondary_group_ids, is_banned, is_staff, secret_key FROM xf_user WHERE user_id = "' . SanitizeInput( $_POST[ "id" ] ) . '"',
+                                => 'SELECT secondary_group_ids, is_banned, is_staff, secret_key FROM xf_user WHERE user_id = "' . SanitizeInput( Decrypt( $_POST[ $identifiers[ "id" ] ] ) ) . '"',
                             "xf_user_upgrade_active"
-                                => 'SELECT end_date FROM xf_user_upgrade_active WHERE user_id = "' . SanitizeInput( $_POST[ "id" ] ) . '"',
+                                => 'SELECT end_date FROM xf_user_upgrade_active WHERE user_id = "' . SanitizeInput( Decrypt( $_POST[ $identifiers[ "id" ] ] ) ) . '"',
                             "xf_rm_resource"
-                                => 'SELECT current_version_id FROM xf_rm_resource WHERE title = "' . SanitizeInput( $_POST[ "ext" ] ) . '"',
+                                => 'SELECT current_version_id FROM xf_rm_resource WHERE title = "' . SanitizeInput( Decrypt( $_POST[ $identifiers[ "ext" ] ] ) ) . '"',
                             "xf_user_field_value"
-                                => 'SELECT field_value FROM xf_user_field_value WHERE field_id = "unique_id", user_id = "' . SanitizeInput( $_POST[ "id" ] ) . '"',
+                                => 'SELECT field_value FROM xf_user_field_value WHERE field_id = "unique_id", user_id = "' . SanitizeInput( Decrypt( $_POST[ $identifiers[ "id" ] ] ) ) . '"',
                             "xf_loader_atmp"
                                 => 'SELECT * FROM xf_loader_atmp');
 $results        = array( "xf_user"
@@ -70,13 +79,13 @@ if ( $results[ "xf_user" ]->num_rows > 0 &&
             ( $rows[ "xf_user_upgrade_active" ][ "end_date" ] > time( ) ||
                 $rows[ "xf_user_upgrade_active" ][ "end_date" ] == 0 ) )
     {
-        if ( $rows[ "xf_user" ][ "secret_key" ] == SanitizeInput( $_POST[ "sk" ] ) )
+        if ( $rows[ "xf_user" ][ "secret_key" ] == SanitizeInput( Decrypt( $_POST[ $identifiers[ "sk" ] ] ) ) )
         {
-            if ( $rows[ "xf_user_field_value" ][ "field_value" ] == SanitizeInput( $_POST[ "uid" ] ) )
+            if ( $rows[ "xf_user_field_value" ][ "field_value" ] == SanitizeInput( Decrypt( $_POST[ $identifiers[ "uid" ] ] ) ) )
             {
 				StartSession( );
-                if ( $rows[ "xf_user" ][ "is_staff" ] == 1 ) // if ( SanitizeInput( $_POST[ "dll" ] ) )
-                    die( ReturnKeys[ "Staff Success" ] ); // { send = compile_dll( SanitizeInput( $_POST[ "ext" ] ) ); }
+                if ( $rows[ "xf_user" ][ "is_staff" ] == 1 ) // if ( SanitizeInput( Decrypt( $_POST[ "dll" ] ) ) )
+                    die( ReturnKeys[ "Staff Success" ] ); // { send = compile_dll( SanitizeInput( Decrypt( $_POST[ "ext" ] ) ) ); }
                 die( ReturnKeys[ "Success" ] );
             }
             die( ReturnKeys[ "Identity Mismatch" ] );
--- Website/include.php
+++ Website/include.php
@@ -3,6 +3,7 @@
 	// TODO: Logging include file.
 	// TODO: Encrypting and decrypting and generating encryption keys, ivs and post identifiers.
 	// TODO: Check loader hash.
+	
 	define("ReturnKeys",
 		array( "Invalid Hash"			=> "-1",
 		"Establishing Failure" 			=> "0", // Couldn't connect / set up database info
@@ -14,6 +15,35 @@
 	
 	define( "SQL", array( "ServerName" => "localhost", "DatabaseUsername" => "palatfef_admin", "DatabasePassword" => "", "DatabaseName" => "palatfef_xenforo" ) );
 	$sql_connection = new mysqli( SQL[ "ServerName" ], SQL[ "DatabaseUsername" ], SQL[ "DatabasePassword" ], SQL[ "DatabaseName" ] );
+	
+	function GenerateKeys( )
+	{
+		$time = microtime(false);
+		$time = substr($time, 11, strlen($time) - 11);
+		$time = floor($time / 10);
+		$keys[ "iv" ] = substr(md5($time), 0, 16);
+		$keys[ "enc_key" ] = $keys[ "iv " ] . $keys[ "iv" ];
+		return $keys;
+	}
+	
+	define( "Keys", GenerateKeys( ) );
+	
+	function GenerateIdentifier( $plain_text ) // Anything indexing the $_POST global needs to be one of these identifiers.
+	{
+		return substr( md5( md5( global Keys[ "iv" ] ) . md5( $plain_text) ), 4, 14); // Start and finish can be random, as long as they match the client
+	}
+	
+	define( "EncryptionMethod", "AES-256-CBC" );
+	
+	function Encrypt( $plain_text )
+	{
+		return openssl_encrypt( $plain_text, EncryptionMethod, Keys[ "enc_key" ], 0, Keys[ "iv" ] ); // Base64 encode then encrypt.
+	}
+	
+	function Decrypt( $cipher_text )
+	{
+		return openssl_decrypt( $cipher_text, EncryptionMethod, Keys[ "enc_key" ], 0, Keys[ "iv" ] ); // Base64 decode then decrypt.
+	}
 	
 	define( "UnsafeKeywords", array( " drop ", " delete " ) ); // TODO: Add all unsafe keywords.
 	
@@ -39,8 +69,6 @@
 	define( "GameID", array( "CS: GO" => 2, "PUBG" => 3 ) );
 	define( "GameInfo", array( 2 => ExtensionDirectory . "csgo.info", 3 => ExtensionDirectory . "pubg.info" ) );
 	define( "GameCheat", array( 2=> ExtensionDirectory . "csgo.dll", 3 => ExtensionDirectory . "pubg.dll" ) );
-	
-	define( "EncryptionMethod", "AES-256-CBC" );
 	
 	function SendCheat( $game_id )
 	{
