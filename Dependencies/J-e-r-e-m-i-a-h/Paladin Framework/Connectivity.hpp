7d8ba8670c38ad58460d40a51dd7c248bddca88a
Cole <gtxzombie25@gmail.com>
2018-05-10 17:59:12 -0600

    Work on Connectivity

--- Dependencies/J-e-r-e-m-i-a-h/Paladin Framework/Connectivity.hpp
+++ Dependencies/J-e-r-e-m-i-a-h/Paladin Framework/Connectivity.hpp
@@ -7,9 +7,20 @@ namespace Paladin
 {
 	class CConnectivity;
 	extern CConnectivity con;
-
+	
 	class CConnectivity
 	{
+		enum EConnectionCodes
+		{
+			INVALID_HASH = -1,
+			ESTABLISHING_FAILURE,
+			BANNED,
+			IDENTITY_MISMATCH,
+			INACTIVE_PREMIUM,
+			STAFF_SUCCESS,
+			SUCCESS
+		}
+		
 		int GetUniqueID( )
 		{
 #define UNLEN 256
@@ -23,7 +34,7 @@ namespace Paladin
 		}
 
 		std::string strPostFields;
-		void InitializeData( bool bSendDLL, std::string strExtension ) // bSendDLL will determine whether you'd like to send the DLL or just DLL initialization information (e.g. signatures)
+		void InitializeData( bool bSendDLL, int iExtension ) // bSendDLL will determine whether you'd like to send the DLL or just DLL initialization information (e.g. signatures)
 		{
 			std::ifstream ifRegistration( XOR( "registration.key" ) );
 			std::string strUserID;
@@ -32,11 +43,11 @@ namespace Paladin
 			std::getline( ifRegistration, strSecretKey );
 
 			std::string strEncryptionKey = enc.Hash_SHA3_256( std::to_string( std::chrono::duration_cast< std::chrono::seconds >( std::chrono::system_clock::now( ).time_since_epoch( ) ).count( ) / 10 ).c_str( ) ).substr( 0, 32 );
-			strPostFields = enc.Hash_SHA3_256( enc.Encrypt( XOR( "id" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( strUserID, strEncryptionKey ) // user_id
-							+ XOR( "&" ) + enc.Hash_SHA3_256( enc.Encrypt( XOR( "uid" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( std::to_string( GetUniqueID( ) ), strEncryptionKey ) // unique_id
-							+ XOR( "&" ) + enc.Hash_SHA3_256( enc.Encrypt( XOR( "sk" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( strSecretKey, strEncryptionKey ) // secret_key
-							+ XOR( "&" ) + enc.Hash_SHA3_256( enc.Encrypt( XOR( "dll" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( std::to_string( bSendDLL ), strEncryptionKey ) // bool if requested to send dll
-							+ XOR( "&" ) + enc.Hash_SHA3_256( enc.Encrypt( XOR( "ext" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( strExtension, strEncryptionKey ); // requested extension
+			strPostFields = enc.Hash_SHA3_256( enc.Encrypt( XOR( "file" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( std::to_string( !bSendDLL ), strEncryptionKey ) // user_id
+							//+ XOR( "&" ) + enc.Hash_SHA3_256( enc.Encrypt( XOR( "uid" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( std::to_string( GetUniqueID( ) ), strEncryptionKey ) // unique_id
+							//+ XOR( "&" ) + enc.Hash_SHA3_256( enc.Encrypt( XOR( "sk" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( strSecretKey, strEncryptionKey ) // secret_key
+							//+ XOR( "&" ) + enc.Hash_SHA3_256( enc.Encrypt( XOR( "dll" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( std::to_string( bSendDLL ), strEncryptionKey ) // bool if requested to send dll
+							+ XOR( "&" ) + enc.Hash_SHA3_256( enc.Encrypt( XOR( "ext" ), strEncryptionKey ) + strEncryptionKey ) + XOR( "=" ) + enc.Encrypt( std::stoi( iExtension ), strEncryptionKey ); // requested extension
 
 		}
 		void InitializeData( ) // used only for verification of authenticity
@@ -70,13 +81,10 @@ namespace Paladin
 			curl_easy_setopt( pvCurl, CURLOPT_WRITEDATA, &strResponseBuffer );
 			curl_easy_perform( pvCurl );
 			curl_easy_cleanup( pvCurl );
-			if ( strResponseBuffer.length( ) == 1 )
+			if ( strResponseBuffer.length( ) )
 				return std::stoi( strResponseBuffer );
 			else
-			{
-				pvDLL = new const char * { strResponseBuffer.c_str( ) }; // TODO decrypt DLL
-				return 0;
-			}
+				return ESTABLISHING_FAILURE;
 		}
 
 	public:
@@ -98,7 +106,8 @@ namespace Paladin
 			{
 				do
 				{
-					if ( con.Authenticate( ) != 0 )
+					auto iReturnCode = con.Authenticate( );
+					if ( !( iReturnCode == STAFF_SUCCESS || iReturnCode == SUCCESS ) )
 					{
 						bReturn = true;
 						std::terminate( );
