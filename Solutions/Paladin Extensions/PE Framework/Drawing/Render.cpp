64ecfdd5b4e6ef64e61463028a40c925adb2cdd7
Cole <gtxzombie25@gmail.com>
2018-06-09 20:28:39 -0600

    Added Rendering to Framework

--- Solutions/Paladin Extensions/PE Framework/Drawing/Render.cpp
+++ Solutions/Paladin Extensions/PE Framework/Drawing/Render.cpp
@@ -5,5 +5,64 @@
 
 namespace Paladin
 {
+	void CRender::CreateRenderTarget( ) 
+	{
+		RegisterClassExW( &wndWindow );
+		hWnd = CreateWindowExW( 0, szWindowTitle, szWindowTitle, WS_POPUP,
+							   iWindowStartPosition[ 0 ], iWindowStartPosition[ 1 ], uWindowWidth, uWindowHeight,
+							   nullptr, nullptr, wndWindow.hInstance, nullptr );
+		//SetLayeredWindowAttributes( hWnd, 0, 1.0f, LWA_ALPHA );
+		//SetLayeredWindowAttributes( hWnd, 0, 0, LWA_COLORKEY );
+		ShowWindow( hWnd, SW_SHOWDEFAULT );
+		UpdateWindow( hWnd );
+	}
 
+	void CRender::InitializeDirectX( ) 
+	{
+		Direct3DCreate9Ex( D3D_SDK_VERSION, &pObjectEx );
+
+		dxParameters.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;
+		dxParameters.BackBufferWidth = uWindowWidth;
+		dxParameters.BackBufferHeight = uWindowHeight;
+		dxParameters.BackBufferFormat = D3DFMT_X8R8G8B8;
+		dxParameters.BackBufferCount = 1;
+		dxParameters.MultiSampleType = D3DMULTISAMPLE_NONE;
+		dxParameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
+		dxParameters.hDeviceWindow = hWnd;
+		dxParameters.EnableAutoDepthStencil = true;
+		dxParameters.AutoDepthStencilFormat = D3DFMT_D24S8;
+		dxParameters.Flags = D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL;
+		dxParameters.Windowed = true;
+
+		pObjectEx->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &dxParameters, &pDevice );
+		pDevice->GetCreationParameters( &cpCreationParameters );
+	}
+
+	void CRender::BeginRender( ) 
+	{
+		pDevice->Clear( 0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER | D3DCLEAR_STENCIL, D3DCOLOR_ARGB( 0, 0, 0, 0 ), 0, 0 );
+		pDevice->BeginScene( );
+	}
+
+	void CRender::EndRender( )
+	{
+		pDevice->EndScene( );
+		pDevice->Present( nullptr, nullptr, nullptr, nullptr );
+	}
+
+	void CRender::SetWindowSize( unsigned uWidth, unsigned uHeight ) 
+	{
+		uWindowWidth = uWidth;
+		uWindowHeight = uHeight;
+
+		if ( !hWnd )
+			return;
+
+		RECT rcWindowRect;
+		GetWindowRect( hWnd, &rcWindowRect );
+		auto rcNewWindowRect = rcWindowRect;
+		rcNewWindowRect.bottom += uHeight - ( rcWindowRect.bottom - rcWindowRect.top );
+		rcNewWindowRect.right += uWidth - ( rcWindowRect.right - rcWindowRect.left );
+		SetWindowPos( hWnd, nullptr, rcWindowRect.left, rcWindowRect.top, uWindowWidth, uWindowHeight, SWP_SHOWWINDOW );
+	}
 }
--- Solutions/Paladin Extensions/PE Framework/Drawing/Render.hpp
+++ Solutions/Paladin Extensions/PE Framework/Drawing/Render.hpp
@@ -4,9 +4,34 @@
 
 namespace Paladin
 {
+	LRESULT WINAPI WndProc( HWND, UINT, WPARAM, LPARAM );
+
 	class CRender
 	{
-	public:
+	protected:
+		IDirect3D9Ex					*pObjectEx { };
+		D3DDEVICE_CREATION_PARAMETERS	cpCreationParameters { };
 
+		HWND			hWnd { nullptr };
+		unsigned		uWindowWidth { }, uWindowHeight { };
+		const unsigned  uTitleBarHeight { 25 };
+		const long		uWindowStyle { WS_EX_TRANSPARENT | WS_EX_TOPMOST | WS_EX_NOACTIVATE | WS_EX_LAYERED };
+
+		const unsigned	uScreenWidth = GetSystemMetrics( SM_CXSCREEN ), uScreenHeight = GetSystemMetrics( SM_CYSCREEN );
+		const int		iWindowStartPosition[ 2 ] { ( uScreenWidth / 2 ) - ( uWindowWidth / 2 ), ( uScreenHeight / 2 ) - ( uWindowHeight / 2 ) };
+		wchar_t			*szWindowTitle { static_cast< wchar_t* >( malloc( 32 ) ) };
+		WNDCLASSEX		wndWindow { sizeof( WNDCLASSEX ), NULL, WndProc , 0, 0, GetModuleHandle( nullptr ), nullptr,
+			LoadCursor( nullptr, IDC_ARROW ), nullptr, nullptr, szWindowTitle, nullptr };
+
+		void CreateRenderTarget( );
+		virtual void InitializeDirectX( );
+		void BeginRender( );
+		void EndRender( );
+	public:
+		virtual ~CRender( ) = default;
+		IDirect3DDevice9		*pDevice { };
+		D3DPRESENT_PARAMETERS	dxParameters { };
+		
+		void SetWindowSize( unsigned, unsigned );
 	};
 }
