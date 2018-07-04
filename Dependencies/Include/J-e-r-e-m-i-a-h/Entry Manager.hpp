86c4b20ffe80d6ce69a23858b0566e0946559c60
voiding <nsbevids@gmail.com>
2018-07-03 22:32:30 -0700

    Updated Dependencies Entry Manager
    Now follows MSDN guideline for entry down to the letter.

--- Dependencies/Include/J-e-r-e-m-i-a-h/Entry Manager.hpp
+++ Dependencies/Include/J-e-r-e-m-i-a-h/Entry Manager.hpp
@@ -17,9 +17,9 @@ namespace Paladin
 extern void OnLaunch( );
 
 #if defined( UNICODE )
-int WINAPI wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
+int APIENTRY wWinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow )
 #else
-int WINAPI WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow )
+int APIENTRY WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow )
 #endif
 {
 #if defined( UNICODE )
