39bad8fe4d038930ef0efc8e8d55d532576d7729
MiKe <32632841+atleast3characterslong@users.noreply.github.com>
2018-07-14 18:53:16 -0700

    Injector Paths
    Added include and library paths for injector project.

--- Solutions/Paladin Extensions Test/PE Injector/main.hpp
+++ Solutions/Paladin Extensions Test/PE Injector/main.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <J-e-r-e-m-i-a-h/Standard Library.hpp>
+
+#define PX_ENTRY_AS_WIN
+#include <J-e-r-e-m-i-a-h/Entry Manager.hpp>
+
+#include <Framework.hpp>
+using namespace Paladin;
