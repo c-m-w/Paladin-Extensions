a6f83262c7046154f4bd0dcc5ef593757e2d5919
voiding <nsbevids@gmail.com>
2019-01-09 21:20:14 -0700

    Update Dependencies

--- Dependencies/Include/cryptopp/salsa.h
+++ Dependencies/Include/cryptopp/salsa.h
@@ -9,12 +9,22 @@
 #include "strciphr.h"
 #include "secblock.h"
 
-// "Inline assembly operands don't work with .intel_syntax", http://llvm.org/bugs/show_bug.cgi?id=24232
-#if CRYPTOPP_BOOL_X32 || defined(CRYPTOPP_DISABLE_INTEL_ASM)
-# define CRYPTOPP_DISABLE_SALSA_ASM
+// Clang 3.3 integrated assembler crash on Linux. Clang 3.4 due to compiler
+// error with .intel_syntax, http://llvm.org/bugs/show_bug.cgi?id=24232
+#if CRYPTOPP_BOOL_X32 || defined(CRYPTOPP_DISABLE_MIXED_ASM)
+# define CRYPTOPP_DISABLE_SALSA_ASM 1
 #endif
 
 NAMESPACE_BEGIN(CryptoPP)
+
+/// \brief Salsa20 core transform
+/// \param data the data to transform
+/// \param rounds the number of rounds
+/// \details Several algorithms, like CryptoBox and Scrypt, require access to
+///   the core Salsa20 transform. The current Crypto++ implementation does not
+///   lend itself to disgorging the Salsa20 cipher from the Salsa20 core transform.
+///   Instead Salsa20_Core is provided with customary accelerations.
+void Salsa20_Core(word32* data, unsigned int rounds);
 
 /// \brief Salsa20 stream cipher information
 struct Salsa20_Info : public VariableKeyLength<32, 16, 32, 16, SimpleKeyingInterface::UNIQUE_IV, 8>
@@ -31,10 +41,13 @@ protected:
 	void CipherResynchronize(byte *keystreamBuffer, const byte *IV, size_t length);
 	bool CipherIsRandomAccess() const {return true;}
 	void SeekToIteration(lword iterationCount);
+
 #if (CRYPTOPP_BOOL_X86 || CRYPTOPP_BOOL_X32 || CRYPTOPP_BOOL_X64)
 	unsigned int GetAlignment() const;
 	unsigned int GetOptimalBlockSize() const;
 #endif
+
+	std::string AlgorithmProvider() const;
 
 	FixedSizeAlignedSecBlock<word32, 16> m_state;
 	int m_rounds;
