546bcd96985e36b6e9cd0e96e9b3980132eb3c3f
voiding <nsbevids@gmail.com>
2019-02-06 16:23:36 -0700

    Update VMProtect
    Basic project files, autocopy bat.

---
+++ Source/VMProtect/Xcopy Release Files.bat
@@ -0,0 +1,10 @@
+@echo off
+
+Xcopy ..\Release\Launcher.exe . /c /v /q /y
+Xcopy ..\Release\Launcher.map . /c /v /q /y
+
+Xcopy ..\Release\Client.dll . /c /v /q /y
+Xcopy ..\Release\Client.map . /c /v /q /y
+
+Xcopy ..\Release\Setup.exe . /c /v /q /y
+Xcopy ..\Release\Setup.map . /c /v /q /y
