e9f7039c304d27d49eb47d3490277314f8be6256
voiding <nsbevids@gmail.com>
2019-02-06 19:28:00 -0700

    Added CSGOX

--- Source/VMProtect/Xcopy Release Files.bat
+++ Source/VMProtect/Xcopy Release Files.bat
@@ -8,3 +8,6 @@ Xcopy ..\Release\Client.map . /c /v /q /y
 
 Xcopy ..\Release\Setup.exe . /c /v /q /y
 Xcopy ..\Release\Setup.map . /c /v /q /y
+
+Xcopy ..\Release\CSGOX.dll . /c /v /q /y
+Xcopy ..\Release\CSGOX.map . /c /v /q /y
