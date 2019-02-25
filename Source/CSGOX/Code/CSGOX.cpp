123461e83022e5c1b04554ea991d93badb335100
c-m-w <GTXZombie25@gmail.com>
2019-02-24 18:37:47 -0700

    wow thats a lot of progress 2

--- Source/CSGOX/Code/CSGOX.cpp
+++ Source/CSGOX/Code/CSGOX.cpp
@@ -3,11 +3,14 @@
 #include "Precompile.hpp"
 
 #define DLL_ENTRY
+#define USE_NAMESPACES
+#define USE_DEFINITIONS
 #include "../../Framework/Code/Framework.hpp"
 
 BOOL OnAttach( )
 {
-	if ( !SetupFramework( ) )
+	if ( !SetupFramework( )
+		 || !AUTH.CompareHash( ELibrary::CSGOX, image_info_t( GetModuleHandle( "csgo.exe" ) ).GenerateUniqueHash( ) ) )
 		return FALSE;
 
 	return TRUE;
