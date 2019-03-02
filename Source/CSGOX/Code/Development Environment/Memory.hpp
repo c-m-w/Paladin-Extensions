7242dca0fb1e4c1efa4aab09d1cd54876ae8cfb4
c-m-w <GTXZombie25@gmail.com>
2019-03-01 18:09:41 -0700

    Update Memory.hpp

--- Source/CSGOX/Code/Development Environment/Memory.hpp
+++ Source/CSGOX/Code/Development Environment/Memory.hpp
@@ -1,4 +1,4 @@
-/// Interfaces.hpp
+/// Memory.hpp
 
 #pragma once
 
@@ -36,18 +36,18 @@ namespace Modules
 	};
 
 	inline module_info_t _Modules[ MODULE_MAX ];
-	inline module_info_t &_Client = _Modules[ MODULE_CLIENT ];
-	inline module_info_t &_Engine = _Modules[ MODULE_ENGINE ];
-	inline module_info_t &_Direct3D = _Modules[ MODULE_DIRECT_3D ];
-	inline module_info_t &_Direct3DAPI = _Modules[ MODULE_DIRECT_3D_API ];
-	inline module_info_t &_Overlay = _Modules[ MODULE_OVERLAY ];
-	inline module_info_t &_VGUI = _Modules[ MODULE_VGUI ];
-	inline module_info_t &_VGUI2 = _Modules[ MODULE_VGUI2 ];
-	inline module_info_t &_InputSystem = _Modules[ MODULE_INPUTSYSTEM ];
-	inline module_info_t &_MaterialSystem = _Modules[ MODULE_MATERIALSYSTEM ];
-	inline module_info_t &_ValveStandardLibrary = _Modules[ MODULE_VSTLIB ];
-	inline module_info_t &_Localize = _Modules[ MODULE_LOCALIZE ];
-	inline module_info_t &_Filesystem = _Modules[ MODULE_FILESYSTEM ];
+	inline module_info_t &_Client				= _Modules[ MODULE_CLIENT ];
+	inline module_info_t &_Engine				= _Modules[ MODULE_ENGINE ];
+	inline module_info_t &_Direct3D				= _Modules[ MODULE_DIRECT_3D ];
+	inline module_info_t &_Direct3DAPI			= _Modules[ MODULE_DIRECT_3D_API ];
+	inline module_info_t &_Overlay				= _Modules[ MODULE_OVERLAY ];
+	inline module_info_t &_VGUI					= _Modules[ MODULE_VGUI ];
+	inline module_info_t &_VGUI2				= _Modules[ MODULE_VGUI2 ];
+	inline module_info_t &_InputSystem			= _Modules[ MODULE_INPUTSYSTEM ];
+	inline module_info_t &_MaterialSystem		= _Modules[ MODULE_MATERIALSYSTEM ];
+	inline module_info_t &_ValveStandardLibrary	= _Modules[ MODULE_VSTLIB ];
+	inline module_info_t &_Localize				= _Modules[ MODULE_LOCALIZE ];
+	inline module_info_t &_Filesystem			= _Modules[ MODULE_FILESYSTEM ];
 }
 
 namespace Interfaces
@@ -70,7 +70,8 @@ public:
 	static inline std::string strModules[ Modules::MODULE_MAX ]
 	{
 		ENC( "client_panorama.dll" ),
-		ENC( "engine.dll" ),
+		ENC( "engine.dll" ),		
+		ENC( "d3d9.dll" ),			
 		ENC( "shaderapidx9.dll" ),
 		ENC( "GameOverlayRenderer.dll" ),
 		ENC( "vguimatsurface.dll" ),
@@ -90,9 +91,9 @@ public:
 	{
 		Interfaces::pDevice = **reinterpret_cast< decltype( Interfaces::pDevice )** >( Interfaces::pDevice );
 	} );
-	static inline pattern_t _ResetPattern = pattern_t( ENC( "CC E9 ? ? ? ? 83 E4 F8 81 EC + 1" ), &pReset, 1 );
+	static inline pattern_t _ResetPattern = pattern_t( ENC( "CC E9 ? ? ? ? 83 E4 F8 81 EC" ), &pReset, 1 );
 	static inline pattern_t _BeginScenePattern = pattern_t( ENC( "5F 5E EB CA CC" ), &pBeginScene, 5 );
-	static inline pattern_t _EndScenePattern = pattern_t( ENC( "83 E0 01 5D C2 04 00 CC" ), &pEndScene, 15 );
+	static inline pattern_t _EndScenePattern = pattern_t( ENC( "CC CC 6A 18 B8 ? ? ? ? E8 ? ? ? ? 8B 7D 08 8B DF" ), &pEndScene, 2 );
 
 #else
 
