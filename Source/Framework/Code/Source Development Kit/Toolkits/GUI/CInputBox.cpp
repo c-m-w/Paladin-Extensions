662db81e14cc1b208f921561fdd503498ed71a05
c-m-w <GTXZombie25@gmail.com>
2019-08-28 15:01:37 -0600

    l

--- Source/Framework/Code/Source Development Kit/Toolkits/GUI/CInputBox.cpp
+++ Source/Framework/Code/Source Development Kit/Toolkits/GUI/CInputBox.cpp
@@ -6,4 +6,54 @@
 #define USE_NAMESPACES
 #include "../../../Framework.hpp"
 
+void CInputBox::CreateDrawables( )
+{}
 
+CInputBox::CInputBox( ) :
+	IWidget( WIDGET_BUTTON, CURSOR_IBEAM )
+{ 
+	SetColor( COLOR_INDEX_PRIMARY, STATE_DORMANT, TEXT_DARK );
+	SetColor( COLOR_INDEX_PRIMARY, STATE_ACTIVATED, TEXT_NORMAL );
+	SetColor( COLOR_INDEX_PRIMARY, STATE_HOVERING, TEXT_NORMAL );
+	SetColor( COLOR_INDEX_PRIMARY, STATE_CLICKING, TEXT_NORMAL );
+	SetColor( COLOR_INDEX_SECONDARY, STATE_DORMANT, BACKGROUND_DEFAULT );
+	SetColor( COLOR_INDEX_SECONDARY, STATE_ACTIVATED, BACKGROUND_DEFAULT );
+	SetColor( COLOR_INDEX_SECONDARY, STATE_HOVERING, BACKGROUND_DEFAULT );
+	SetColor( COLOR_INDEX_SECONDARY, STATE_CLICKING, BACKGROUND_DEFAULT );
+	SetColor( COLOR_INDEX_TERTIARY, STATE_DORMANT, BLUE );
+}
+
+void CInputBox::AddFilter( EFilter _NewFilter )
+{
+	*reinterpret_cast< unsigned* >( &_CurrentFilter ) |= _NewFilter;
+}
+
+void CInputBox::RemoveFilter( EFilter _FilterToRemove )
+{
+	*reinterpret_cast< unsigned* >( &_CurrentFilter ) &= ~_FilterToRemove;
+}
+
+void CInputBox::NewCharacter( char chInsertion )
+{
+	if ( strBuffer.length( ) >= zMaxLength )
+		return;
+
+	if ( _CurrentMode == MODE_INSERT )
+		strBuffer.insert( strBuffer.begin( ) + uInsertionPoint, chInsertion );
+	else
+		strBuffer[ uInsertionPoint ] = chInsertion;
+}
+
+void CInputBox::SetMaxLength( std::size_t zNewLength )
+{
+	zMaxLength = zNewLength;
+}
+
+void CInputBox::OnKeyTyped( char chCharacter )
+{
+	if ( _CurrentFilter & FILTER_LETTERS && FILTERS[ int( log( double( FILTER_LETTERS ) ) / log( 2.0 ) ) ]( chCharacter )
+		 || _CurrentFilter & FILTER_NUMERIC && FILTERS[ int( log( double( FILTER_NUMERIC ) / log( 2.0 ) ) ) ]( chCharacter )
+		 || _CurrentFilter & FILTER_SPECIAL_CHARACTERS && FILTERS[ int( log( double( FILTER_SPECIAL_CHARACTERS ) ) / log( 2.0 ) ) ]( chCharacter )
+		 || _CurrentFilter & FILTER_SPECIAL_CHARACTERS && FILTERS[ int( log( double( FILTER_SPACE ) ) / log( 2.0 ) ) ]( chCharacter ) )
+		NewCharacter( chCharacter );
+}
--- Source/Framework/Code/Source Development Kit/Toolkits/GUI/CInputBox.hpp
+++ Source/Framework/Code/Source Development Kit/Toolkits/GUI/CInputBox.hpp
@@ -4,5 +4,71 @@
 
 class CInputBox: public IWidget
 {
+public:
+
+	enum EFilter: unsigned
+	{
+		FILTER_NONE					= 0,
+		FILTER_LETTERS				= 1 << 0,
+		FILTER_NUMERIC				= 1 << 1,
+		FILTER_SPECIAL_CHARACTERS	= 1 << 2,
+		FILTER_SPACE				= 1 << 3
+	};
+
+	enum EMode: unsigned
+	{
+		MODE_INSERT,
+		MODE_REPLACE
+	};
 	
+private:
+
+	void CreateDrawables( ) override;
+
+	std::string strBuffer { };
+	unsigned uInsertionPoint = 0u;
+	unsigned uFirstInsertionPointVisible = 0u;
+	int iSelection = 0;
+	EFilter _CurrentFilter = FILTER_NONE;
+	EMode _CurrentMode = MODE_INSERT;
+	std::size_t zMaxLength = 32u;
+	
+public:
+	
+	static constexpr auto TEXT_HEIGHT = 0.125;
+	static constexpr auto FONT = FONT_ROBOTO_MONO;
+	static constexpr auto FONT_WEIGHT = WEIGHT_REGULAR;
+	static constexpr auto ROUNDING = 0.1;
+
+	static constexpr auto FILTERS[ 4 ]
+	{
+		[ ]( char chCharacter ) // FILTER_LETTERS
+		{
+			return chCharacter >= 'A' && chCharacter <= 'Z'
+				|| chCharacter >= 'a' || chCharacter <= 'z';
+		},
+		[ ]( char chCharacter ) // FILTER_NUMERIC
+		{
+			return chCharacter >= '0' && chCharacter <= '9';
+		},
+		[ ]( char chCharacter ) // FILTER_SPECIAL_CHARACTERS
+		{
+			return chCharacter >= '!' && chCharacter <= '/'
+				|| chCharacter >= ':' && chCharacter <= '@'
+				|| chCharacter >= '[' && chCharacter <= '`'
+				|| chCharacter >= '{' && chCharacter <= '~';
+		},
+		[ ]( char chCharacter ) // FILTER_SPACE
+		{
+			return chCharacter == ' ';
+		}
+	};
+	
+	CInputBox( );
+
+	void AddFilter( EFilter _NewFilter );
+	void RemoveFilter( EFilter _FilterToRemove );
+	void NewCharacter( char chInsertion );
+	void SetMaxLength( std::size_t zNewLength );
+	void OnKeyTyped( char chCharacter ) override;
 };
